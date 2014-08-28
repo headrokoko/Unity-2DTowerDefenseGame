7105284ec041879eae93046a21a9725264c3fac4
headrokoko <supermerio1008@hotmail.co.jp>
2014-08-28 12:10:01 +0900

    20140828プレイヤーコントロールを追加
    ゲーム企画のメモ、敵の行動パターンメモ等も追加

---
+++ Assets/Code/Script/PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float playerSpeed;
+	public float JumpPower;
+	private float Movepow;
+	private float MovePowMin = 2;
+	private bool OnFloor = false;
+	CapsuleCollider BodyColider;
+
+
+	void Awake(){
+		this.BodyColider = GetComponent<CapsuleCollider>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(IsFloor());
+
+		if((Input.anyKey) & OnFloor){
+			//スペース押したらジャンプ
+			if(Input.GetKeyDown(KeyCode.Space)){
+				rigidbody.AddForce(Vector3.up * JumpPower);
+			}
+			//画面左右の動き
+			Movepow = MovePowMin + Input.GetAxis("Horizontal") * playerSpeed;
+			rigidbody.AddForce(Movepow,0,0);
+		}
+	}
+
+	bool IsFloor(){
+		return Physics.Raycast(transform.position,new Vector3(0,1,0),this.BodyColider.radius);
+	}
+
+
+	//Floorに着地したとき
+	void OnCollisionEnter(Collision CollisionObj){
+		if(CollisionObj.gameObject.tag == "Floor"){
+			Debug.Log("ONFloor");
+			OnFloor = true;
+		}
+	}
+	//Floor上にいるとき
+	void OnCollisionStay(Collision CollisionObj){
+		if(CollisionObj.gameObject.tag == "Floor"){
+			//Debug.Log("KeepFloor");
+			OnFloor = true;
+		}
+	}
+	//Floorから離れたとき
+	void OnCollisionExit(Collision CollisionObj){
+		if(CollisionObj.gameObject.tag == "Floor"){
+			//Debug.Log("TakeOffFloor");
+			OnFloor = false;
+		}
+	}
+}
