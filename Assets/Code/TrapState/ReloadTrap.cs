0bb122ea6f01f383328c39a10b9eb49437f79643
headrokoko <supermerio1008@hotmail.co.jp>
2014-09-12 19:09:42 +0900

    20140912 トラップのステート遷移の検討
    トラップのFSMの構造を検討中
    指摘されたところの修正

--- Assets/Code/TrapState/ReloadTrap.cs
+++ Assets/Code/TrapState/ReloadTrap.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Searrch : MonoBehaviour {
+public class ReloadTrap : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
