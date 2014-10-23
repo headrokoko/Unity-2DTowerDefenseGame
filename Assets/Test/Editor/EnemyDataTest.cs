9be15158ba994c41d14e7216ea65bfd1fb7f0a57
headrokoko <supermerio1008@hotmail.co.jp>
2014-10-23 21:07:19 +0900

    20141023 EnemyDataTest周りでUnityが起動しなくなることがあったため段階的に変更 3
    EnemyDataTestにTypeテストを追加

--- Assets/Test/Editor/EnemyDataTest.cs
+++ Assets/Test/Editor/EnemyDataTest.cs
@@ -17,7 +17,27 @@ namespace Limone.Test{
 			this.enemydatacontroller = GetEnemyDataMock(IEDataController);
 
 		}
-
+		//Enemy Data Type Test
+		[Test]
+		[Category("Enemy Health Type Test")]
+		public void EnemyHealthTypeTest(){
+			Assert.That(enemydatacontroller.GetEnemyHealth(),Is.TypeOf(typeof(int)));
+		}
+		[Test]
+		[Category("Bullet Damage Type Test")]
+		public void BulletDamageTypeTest(){
+			Assert.That(enemydatacontroller.GetBulletDamage(),Is.TypeOf(typeof(int)));
+		}
+		[Test]
+		[Category("Slip Damage Type Test")]
+		public void SlipDamageTypeTest(){
+			Assert.That(enemydatacontroller.GetSlipDamage(),Is.TypeOf(typeof(int)));
+		}
+		[Test]
+		[Category("Range Type Test")]
+		public void RangeTypeTest(){
+			Assert.That(enemydatacontroller.GetRange(),Is.TypeOf(typeof(float)));
+		}
 
 
 		private IEnemyDataController EnemyDataMock(){
