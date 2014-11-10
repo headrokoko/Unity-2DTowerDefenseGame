032e75daa2859b6596a8d332a3a5e6d52cc9424a
headrokoko <supermerio1008@hotmail.co.jp>
2014-11-10 18:44:53 +0900

    20141110 統合テストでアサーションテストの練習

---
+++ Assets/Code/Script/PassScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		IntegrationTest.Pass(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Code/Script/Powerup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other)
+	{
+		var scale = other.transform.localScale;
+		other.transform.localScale = scale * 2;
+	}
+}
